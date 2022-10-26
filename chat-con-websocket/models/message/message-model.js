2720d96c356ca690f8aebe26da793675778b89e6
jcruzpalermo <jcruzpalermo@gmail.com>
2022-10-25 22:07:59 -0300

    chat-con-websocket

---
+++ chat-con-websocket/models/message/message-model.js
@@ -0,0 +1,5 @@
+import Container from "../../models/container.js";
+
+const Messages = new Container('Messages');
+
+export default Messages;
---
+++ chat-con-websocket/models/product/product-model.js
@@ -0,0 +1,5 @@
+import Container from "../container.js";
+
+const Products = new Container('Products');
+
+export default Products;
